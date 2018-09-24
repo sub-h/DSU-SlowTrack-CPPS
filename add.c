593bc8bb0d3759adf271fbc0eebbd2577c903089
sub-h <42210957+sub-h@users.noreply.github.com>
2018-09-24 18:02:26 +0530

    Update add.c

--- add.c
+++ add.c
@@ -4,9 +4,9 @@ void main()
 // Program to add 2 numbers
 int integer1,integer2,sum;
 printf("Enter the value of the first number\n");
-scanf("%d",integer1);
+scanf("%d",&integer1);
 printf("Enter the value of the second number\n");
-scanf("%d",integer2);
+scanf("%d",&integer2);
 sum=integer1+integer2;
 printf("The sum is %d"sum);
 }
