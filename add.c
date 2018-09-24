1cc409613a01ea02165ceaff368198648c58a10a
sub-h <42210957+sub-h@users.noreply.github.com>
2018-09-24 16:50:55 +0530

    Create add.c

---
+++ add.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+// Program to add 2 numbers
+int integer1,integer2,sum;
+printf("Enter the value of the first number\n");
+scanf("%d",integer1);
+printf("Enter the value of the second number\n");
+scanf("%d",integer2);
+sum=integer1+integer2;
+printf("The sum is %d"sum);
+}
